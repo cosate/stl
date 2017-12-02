993d537d66a6b813db58c3097f942bec7fd0829d
gaofeijin <397830271@qq.com>
2017-12-02 22:49:59 +0800

    little modify

--- include/stl_iterator.h
+++ include/stl_iterator.h
@@ -1,7 +1,7 @@
 #ifndef GRTW_STL_ITERATOR_H
 #define GRTW_STL_ITERATOR_H
 
-#include"stl_iterator.h"
+#include"stl_iterator_base.h"
 
 namespace grtw
 {
@@ -11,7 +11,30 @@ namespace grtw
 	protected:
 		Iterator current;
 	public:
-		using 
+		using iterator_category = typename iterator_traits<Iterator>::iterator_category;
+		using value_type = typename iterator_traits<Iterator>::value_type;
+		using difference_type = typename iterator_traits<Iterator>::difference_type;
+		using pointer = typename iterator_traits<Iterator>::pointer;
+		using reference = typename iterator_traits<Iterator>::reference;
+
+		using _self = reverse_iterator<Iterator>;
+	public:
+		reverse_iterator() {}
+		explicit reverse_iterator(Iterator it) : current(it) {}
+		reverse_iterator(const _self& another) : current(another.current) {}
+
+		Iterator base() const
+		{
+			return current;
+		}
+
+		reference operator*() const
+		{
+			Iterator tmp = current;
+			return *--tmp;
+		}
+
+		
 	};
 }
 
