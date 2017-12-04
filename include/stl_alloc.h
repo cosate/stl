7b46dd24887e00b7b18c1db85af171cebe5fb7c9
gaofeijin <397830271@qq.com>
2017-12-04 16:57:01 +0800

    alloc defination

--- include/stl_alloc.h
+++ include/stl_alloc.h
@@ -2,6 +2,7 @@
 #define GRTW_STL_ALLOC_H
 
 #include<cstdlib>
+#include<new>
 
 namespace grtw
 {
@@ -10,17 +11,75 @@ namespace grtw
 	class malloc_alloc
 	{
 	private:
-		static void oom_malloc(size_t);
-		static void oom_realloc(void*, size_t);
+		static void* oom_malloc(size_t n)
+		{
+			oomh* my_malloc_alloc_handler;
+			void* res;
+
+			while(1)
+			{
+				my_malloc_handler = malloc_alloc_oom_handler;
+				if(0 == my_malloc_handler)
+				{
+					throw std::bad_alloc();
+				}
+				(*my_malloc_handler)();
+				res = malloc(n);
+				if(res)
+					return res;
+			}
+		}
+		static void* oom_realloc(void* p, size_t n)
+		{
+			oomh* my_malloc_handler;
+			void* res;
+			while(1)
+			{
+				my_malloc_handler = malloc_alloc_oom_handler;
+				if(0 == my_malloc_handler)
+				{
+					throw std::bad_alloc();
+				}
+				(*my_malloc_handler)();
+				res = realloc(p, n);
+				if(res)
+					return res;
+			}
+		}
 
 		static oomh* malloc_alloc_oom_handler;
 
 	public:
-		static void* allocate(size_t n);
-		static void deallocate(void*, size_t);
-		static void* reallocate(void* p, size_t, size_t);
-		static oomh* set_malloc_handler(oomh*);
+		static void* allocate(size_t n)
+		{
+			void* res = malloc(n);
+			if(0 == res)
+				res = oom_malloc(n);
+			return res;
+		}
+
+		static void deallocate(void* p, size_t)
+		{
+			free(p);
+		}
+
+		static void* reallocate(void* p, size_t, size_t new_size)
+		{
+			void* res = realloc(p, new_size);
+			if(0 == res)
+				res = oom_realloc(p, new_size);
+			return res;
+		}
+
+		static oomh* set_malloc_handler(oomh* p)
+		{
+			oomh* old = malloc_alloc_oom_handler;
+			malloc_alloc_oom_handler = p;
+			return old;
+		}
 	};
+
+	oomh* malloc_alloc::malloc_alloc_oom_handler = 0;
 
 
 	#define ALIGN 8
@@ -32,7 +91,7 @@ namespace grtw
 	private:
 		union obj
 		{
-			union obj* free_list_link;
+			union obj* next;
 			char client_data[1];
 		};
 		static obj* free_lists[NFREELISTS];
