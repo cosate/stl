231851310a202cc3b6f283a6f4cea592a1902946
gaofeijin <397830271@qq.com>
2017-12-21 22:59:32 +0800

    reconstruct rbtree

--- include/stl_tree.h
+++ include/stl_tree.h
@@ -13,104 +13,37 @@ namespace grtw
 	const RBTreeColorType rb_tree_red = false;
 	const RBTreeColorType rb_tree_black = true;
 
-	struct RBTreeNode_base
+	template<class T>
+	class RBTreeNode
 	{
+	private:
+		T value;
 		RBTreeColorType color;
-		RBTreeNode_base* parent;
-		RBTreeNode_base* left;
-		RBTreeNode_base* right;
+		RBTreeNode* parent;
+		RBTreeNode* left;
+		RBTreeNode* right;
 
-		static RBTreeNode_base* minimum(RBTreeNode_base* root)
-		{
-			while(root->left != nullptr)
-				root = root->left;
-			return root;
-		}
-
-		static RBTreeNode_base* maximum(RBTreeNode_base* root)
-		{
-			while(root->right != nullptr)
-				root = root->right;
-			return root;
-		}
+	public:
+		RBTreeNode() : value(T()), parent(nullptr), left(nullptr), right(nullptr) {}
 	};
 
 	template<class T>
-	struct RBTreeNode : public RBTreeNode_base
+	class RBTree_iterator
 	{
-		T value;
-	};
-
-	struct RBTree_iterator_base
-	{
+	public:
 		using iterator_category = bidireactional_iterator_tag;
 		using difference_type = ptrdiff_t;
-		RBTreeNode_base* node;
-
-		void increment()
-		{
-			if(node->right != nullptr)
-			{
-				node = node->right;
-				while(node->left != nullptr)
-					node = node->left;
-			}
-			else
-			{
-				RBTreeNode_base* p = node->parent;
-				while(node == p->right)
-				{
-					node = p;
-					p = p->parent;
-				}
-				if(node->right != p)
-					node = p;
-			}
-		}
-
-		void decrement()
-		{
-			if(node->color == rb_tree_red && node->parent->parent == node)
-				node = node->right;
-			else if(node->left != nullptr)
-			{
-				RBTreeNode_base* l = node->left;
-				while(l->right != nullptr)
-					l = l->right;
-				node = l;
-			}
-			else
-			{
-				RBTreeNode_base* p = node->parent;
-				while(node == p->left)
-				{
-					node = p;
-					p = p->parent;
-				}
-				node = p;
-			}
-		}
-
-		bool operator==(const RBTree_iterator_base& other)
-		{
-			return node == other.node;
-		}
-
-		bool operator!=(const RBTree_iterator_base& other)
-		{
-			return node != other.node;
-		}
-	};
-
-	template<class T>
-	struct RBTree_iterator : public RBTree_iterator_base
-	{
 		using value_type = T;
 		using reference = T&;
 		using pointer = T*;
+
 		using Self = RBTree_iterator<T>;
 
-		RBTree_iterator() {};
+	private:
+		RBTreeNode<T>* node;
+
+	public:
+		RBTree_iterator() : node(nullptr) {};
 
 		RBTree_iterator(RBTreeNode<T>* x)
 		{
@@ -124,7 +57,7 @@ namespace grtw
 
 		reference operator*() const
 		{
-			return ((RBTreeNode<T>*)node)->value;
+			return node->value;
 		}
 
 		pointer operator->() const
@@ -151,13 +84,105 @@ namespace grtw
 			return *this;
 		}
 
-		Self = operator--(int)
+		Self operator--(int)
 		{
 			Self tmp = *this;
 			decrement();
 			return tmp;
 		}
+
+		bool operator==(const RBTree_iterator& other)
+		{
+			return node == other.node;
+		}
+
+		bool operator!=(const RBTree_iterator& other)
+		{
+			return node != other.node;
+		}
+
+	private:
+		void increment()
+		{
+			if(node->right != nullptr)
+			{
+				node = node->right;
+				while(node->left != nullptr)
+					node = node->left;
+			}
+			else
+			{
+				RBTreeNode* p = node->parent;
+				while(node == p->right)
+				{
+					node = p;
+					p = p->parent;
+				}
+				if(node->right != p)
+					node = p;
+			}
+		}
+
+		void decrement()
+		{
+			if(node->color == rb_tree_red && node->parent->parent == node)
+				node = node->right;
+			else if(node->left != nullptr)
+			{
+				RBTreeNode* l = node->left;
+				while(l->right != nullptr)
+					l = l->right;
+				node = l;
+			}
+			else
+			{
+				RBTreeNode* p = node->parent;
+				while(node == p->left)
+				{
+					node = p;
+					p = p->parent;
+				}
+				node = p;
+			}
+		}
 	};
+
+	template<class Key, class Value class KeyOfValue, class Compare, class Alloc = allocator<T>>
+	class RBTree
+	{
+	public:
+		using value_type = Value;
+		using pointer = value_type*;
+		using const_pointer = const value_type*;
+		using reference = value_type&;
+		using const_reference = const reference;
+		using size_type = size_t;
+		using difference_type = ptrdiff_t;
+
+		using iterator = RBTree_iterator<Value>;
+		using const_iterator = RBTree_iterator<const Value>;
+		using reverse_iterator = reverse_iterator<iterator>;
+		using const_reverse_iterator = reverse_iterator<const_iterator>;
+
+	private:
+		RBTreeNode<Value>* header;
+		size_type node_count;
+		Compare comp;
+
+	private:
+		void empty_initialize()
+		{
+
+		}
+
+	public:
+		RBTree()
+		{
+			header = Alloc::allocator(1);
+			empty_initialize();
+		}
+		RBTree(const Compare& c)
+	}
 }
 
 #endif
